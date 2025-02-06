67b4d8b6a6b73070da96a09d6088fbe2f50a5dba
laksh-krishna-sharma <asklakshsharma@gmail.com>
2025-02-06 17:46:02 +0530

    fixed backend

--- backend/models.py
+++ backend/models.py
@@ -8,12 +8,19 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True, nullable=False)
-    password = Column(String(128), nullable=False) 
-    
+    password = Column(String(128), nullable=False)
+
+    # Define a relationship to the `messages` table
+    messages = relationship("messages", back_populates="user")
+
+# Messages Model
 class messages(Base):
     __tablename__ = "messages"
 
     id = Column(Integer, primary_key=True, index=True)
     labels = Column(String(50), nullable=False)
-    text = Column(String(1000))  
-    user_id = Column(Integer, ForeignKey("users.id"))  
+    text = Column(String(1000))
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    # Define a relationship to the `User` table
+    user = relationship("User", back_populates="messages")
